2e4b149f5fcebfa15c33a19d7062f791d2272c52
Leonardo Marques <leonardo2000marques@gmail.com>
2021-04-28 10:44:55 +0100

    Caixa de testo e atividade de mostrar dados criada

--- app/src/main/java/pt/ipg/exlerdados/MostrarDados.kt
+++ app/src/main/java/pt/ipg/exlerdados/MostrarDados.kt
@@ -2,10 +2,17 @@ package pt.ipg.exlerdados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostrarDados : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_dados)
+
+        val nome =intent.getStringExtra(MainActivity.Info_Extra_Nome)
+
+        val textViewNome = findViewById<TextView>(R.id.textViewNome)
+        textViewNome.setText(nome)
+
     }
 }
